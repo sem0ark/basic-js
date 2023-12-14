8879281bb1301cac66c0d638db1c4355d5b8d12c
sem0ark <arcadii.sem@gmail.com>
2023-12-14 15:39:41 +0100

    feat: complete tasks

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
@@ -19,14 +17,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+function prepareKey(key) {
+  return key.toUpperCase().split('').map((v) => alphabet.indexOf(v));
+}
+
+function moveChar(char, count, key) {
+  if (!alphabet.includes(char)) return char;
+  return alphabet[(alphabet.indexOf(char) + key[count % key.length]) % alphabet.length];
+}
+
+function moveCharBack(char, count, key) {
+  if (!alphabet.includes(char)) return char;
+  return alphabet[
+    (alphabet.length + alphabet.indexOf(char) - key[count % key.length]) % alphabet.length
+  ];
+}
+
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(not_turn = true) {
+    this.reverse = !not_turn;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if (str === undefined || key === undefined)
+      throw new Error("Incorrect arguments!");
+
+    let counter = 0;
+    const k = prepareKey(key);
+
+    const result = str.toUpperCase().split('').map(c => alphabet.includes(c) ? moveChar(c, counter++, k) : c);
+    if (this.reverse) return result.reverse().join('');
+    return result.join('');
+  }
+
+  decrypt(str, key) {
+    if (str === undefined || key === undefined)
+      throw new Error("Incorrect arguments!");
+
+    let counter = 0;
+    const k = prepareKey(key);
+
+    const result = str.toUpperCase().split('').map(c => alphabet.includes(c) ? moveCharBack(c, counter++, k) : c);
+    if (this.reverse) return result.reverse().join('');
+    return result.join('');
   }
 }
 
