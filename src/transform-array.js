24600b7262b92cd04c063269be263a97884fd173
sem0ark <arcadii.sem@gmail.com>
2023-12-14 10:43:24 +0100

    feat: complete minesweaper

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!(arr instanceof Array)) throw Error("'arr' parameter must be an instance of the Array!");
+
+  const result = [];
+  let deletedNext = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-prev') {
+      if (result.length > 0) result.pop();
+    } else if (arr[i] === '--double-next') {
+      if (arr.length > i + 1) result.push(arr[i + 1]);
+    } else if (arr[i] === '--double-prev') {
+      if (i > 0 && !deletedNext) result.push(arr[i - 1]);
+      if (deletedNext) deletedNext = false;
+    } else if (arr[i] === '--discard-next') {
+      i++;
+      deletedNext = true;
+    } else result.push(arr[i]);
+  }
+  return result
 }
 
 module.exports = {
