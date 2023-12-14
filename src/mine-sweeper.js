fea55e1ac3261b532ef44d9f5504130666a019e9
sem0ark <arcadii.sem@gmail.com>
2023-12-14 10:31:28 +0100

    feat: complete minesweaper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const n = matrix.length, m = matrix[0].length;
+
+  const result = new Array(n).fill(null);
+
+  for (let i = 0; i < n; i++) {
+    result[i] = new Array(m).fill(0);
+
+    for (let j = 0; j < m; j++) {
+      let s = 0;
+
+      if (i > 0) s += matrix[i - 1][j];
+      if (j > 0) s += matrix[i][j - 1];
+      if (i < n - 1) s += matrix[i + 1][j];
+      if (j < m - 1) s += matrix[i][j + 1];
+
+      if (i > 0 && j > 0) s += matrix[i - 1][j - 1];
+      if (i > 0 && j < m - 1) s += matrix[i - 1][j + 1];
+      if (i < n - 1 && j > 0) s += matrix[i + 1][j - 1];
+      if (i < n - 1 && j < m - 1) s += matrix[i + 1][j + 1];
+
+      result[i][j] += s;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
